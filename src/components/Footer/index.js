6770497ad1928d12ca7296775834ca0304459a91
fmarcos38 <fmarcos_23@hotmail.com>
2023-08-29 11:23:15 -0300

    land respons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,28 +7,23 @@ function Footer() {
     let whatsapp = `https://wa.me/2281460124`;
 
     return (
-        <div className='contFooter'>
-            <div class="row">
-                <div class="container col-1"></div>
-                <div class="container col-8 footCen">
-                    <div class="container">
+        <div class='row contFooter'>
+                <div class="col-10 footCen">
                         <span className='titulosMed'>Desarrollo Web:</span>
                         <span className='titulosMed'>Marcos Forastiere</span>
-                        <span className='titulosMed'>Full Stack Developer</span>
-                    </div>
+                        <span className='titulosMed'>Full Stack Developer</span>                    
                 </div>
                 
-                <div class="container col-2 footDer">
+                <div class="col-2 footDer">
                     <a href="https://www.instagram.com/fmarcos_casla/">
                         <InstagramIcon className="insta"/>
                     </a>
-                <a href={whatsapp}>
-                    <WhatsAppIcon className="whats"/>
-                </a>                  
+                    <a href={whatsapp}>
+                        <WhatsAppIcon className="whats"/>
+                    </a>                  
                         
                 </div>
-                <div class="container col-1"></div>
-            </div>
+            
         </div>
     )
 }
